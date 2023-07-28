14bc7f12e59e2445868dc0296231d9aee9cdc89a
Javvadi Durga Padma Nagavalli <135938371+JavvadiValli@users.noreply.github.com>
2023-07-28 18:44:30 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,38 @@
+ReactDOM.render(<div>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/PV_Sindhu_headshot.jpg/330px-PV_Sindhu_headshot.jpg"/>
+    <h2>PV Sindhu was born on July 5, 1995, in Hyderabad to PV Ramana (Father) and P Vijaya (Mother). </h2>
+    <h1>EDUCATION:</h1>
+    <h2>PV Sindhu did his schooling at  Auxilium High School, Hyderabad and at St. Ann's College for Women, Hyderabad. Pullela Gopichand, 2001 All England Open Badminton Champion became Sindhu's inspiration to choose badminton as her career. </h2>
+    <h1>PV Sindhu: Badminton Career</h1>
+    <h4>Sindhu at the age of eight started playing badminton. Under the guidance of Mehboob Ali, she started learning the basics of badminton at the badminton courts of the Indian Railway Institute of Signal Engineering and Telecommunications in Secunderabad. She travelled a distance of 56 km every day from her residence to the badminton courts to learn and practice the sport. 
+
+PV Sindhu joined Gopichand Badminton Academy and won several titles under the 10-years category. At the Ambuja Cement All India ranking, she won the 5th Servo All India ranking championship in the doubles and the singles category. 
+
+In the under 13-years category, Sindhu won the titles at the Sub-juniors in Pondicherry, doubles titles at the Krishna Khaitan All India Tournament, IOC All India Ranking, the Sub-Junior Nationals and the All India Ranking in Pune. In the under 14-years category, she won a gold medal at the 51st National State Games in India. 
+
+At the age of 14, PV Sindhu entered the international circuit. She won a bronze medal at the 2009 Sub-Junior Asian Badminton Championships in Colombo. Sindhu won a silver medal at the 2010  Iran Fajr International Badminton Challenge. She made it to the quarterfinals of the 2010 BWF World Junior Championships in Mexico but lost to the Chinese opponent. 
+
+In 2011, Sindhu won Maldives International Challenge in June and the Indonesia  International Challenge in July. At the Dutch Open, she made it to the finals but lost the match. At Swiss International, Sindhu won the Finals beating Carola Bott. She won the India International badminton event in 2011. 
+
+Sindhu is showing great performance at the Tokyo Olympics as well this year in 2021 and has reached the quarterfinals of Women's Single Badminton. </h4>
+    <h1>PV Sindhu: Awards</h1>
+    <h4>1- In January 2020, PV Sindhu was awarded the Highest Civilian Award in India-- Padma Bhushan. </h4>
+
+    <h4>2- In March 2015, Sindhu was awarded the Fourth Highest Civilian Award in India-- Padma Shri.</h4>
+
+    <h4>3- In August 2016, she was awarded the Highest Sporting Honour of India--  Rajiv Gandhi Khel Ratna.</h4>
+
+    <h4>4- In September 2013, PV Sindhu was awarded the Arjuna Award for outstanding performance in sports.</h4> 
+
+    <h4>5- She was titled as the Breakthrough Sportsperson of the Year 2014 by FICCI. </h4>
+
+    <h4>6- In 2014, the NDTV news channel titled her as the NDTV Indian of the Year. </h4>
+
+    <h4>7- She received US$14,000 from the Badminton Association of India for her victory at the 2015 Macau Open Badminton Championships.</h4>
+
+    <h4>8- She received US$7,000 from the Badminton Association of India for her victory in the 2016 Malaysia Masters. </h4>
+
+    <h4>9- In 2019, she was awarded TV9 Nava Nakshatra Sanmanam. </h4>
+    </div>
+    
+    ,document.getElementById("content"));
